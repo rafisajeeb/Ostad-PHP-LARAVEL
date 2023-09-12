514b47cac368c21185554b35bdddfc6016dad01a
rafisajeeb <144761278+rafisajeeb@users.noreply.github.com>
2023-09-12 10:33:31 +0600

    Add files via upload

---
+++ personal_info.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Personal Information</title>
+</head>
+<body>
+
+<?php
+// Define variables
+$name = "Rafi Rahman";
+$age = 32; // Replace with your actual age
+$country = "Bangladesh";
+$introduction = "Hello, I'm Rafi Rahman. I'm a web developer from Bangladesh. Nice to meet you!";
+
+// Display personal information
+echo "<h1>Personal Information</h1>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Age:</strong> $age</p>";
+echo "<p><strong>Country:</strong> $country</p>";
+echo "<p><strong>Introduction:</strong> $introduction</p>";
+?>
+
+</body>
+</html>
